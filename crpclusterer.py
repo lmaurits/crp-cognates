3a4a53486e14e7d2c1ba469d4f34ad66671e7e90
Luke Maurits <luke@maurits.id.au>
2015-07-08 10:42:44 +1200

    Reinstate domain constraints on theta and mus.

--- crpclusterer.py
+++ crpclusterer.py
@@ -187,16 +187,16 @@ class Clusterer:
         """Compute log prior on model parameters."""
 
         # Domain constraints
-#        if not (
-#                (0.5 <= self.theta <= 1.5) and
-#                (0.0 <= self.within_mu <= 1.0) and
-#                (0.0 <= self.between_mu <= 1.0)
-#                ):
-#            prior = safety_log(0.0)
-#            self.prior = prior
-#            return prior
+        if not (
+                (0.0 <= self.theta <= 1.0) and
+                (0.0 <= self.within_mu <= 1.0) and
+                (0.0 <= self.between_mu <= 1.0)
+                ):
+            prior = safety_log(0.0)
+            self.prior = prior
+            return prior
 
-#        prior = 0
+        prior = 0
         # Prior on theta
         # A fairly arbitrary Gamma prior which is basically chosen
         # to trade off between gernally preferring lower theta over higher
